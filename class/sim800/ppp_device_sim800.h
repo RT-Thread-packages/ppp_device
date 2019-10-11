be3706f907605b10aeadceb4602eb0ee207846bd
liuxianliang <liuxianliang@rt-thread.com>
2019-10-11 10:09:04 +0800

    【修改】SIM800重拨功能
    Signed-off-by: liuxianliang <liuxianliang@rt-thread.com>

--- class/sim800/ppp_device_sim800.h
+++ class/sim800/ppp_device_sim800.h
@@ -13,11 +13,14 @@
 
 #include <ppp_device.h>
 
+#define SIM800_WARTING_TIME_BASE 500
+
 /* ppp_device base from ppp_device */
 struct ppp_sim800
 {
     struct ppp_device  device;          /* ppp_device struct in ppp_sim800 */
     enum ppp_trans_type type;           /* the type is used to establish a ppp connection */
+    rt_base_t power_pin;                /* power pin, if device need hardware reset */
 };
 
 extern int ppp_sim800_register(void);
